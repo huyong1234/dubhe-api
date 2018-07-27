46e85cc5f2ce8ab9dccb7c6619c0e7b6de892f5b
wenjiasen <wenjsen@gmail.com>
2018-07-27 14:54:40 +0800

    OSS配置接口

--- app/router.js
+++ app/router.js
@@ -8,4 +8,17 @@ module.exports = (app) => {
   router.get('/', controller.home.index);
 
   router.get('/api/banners', controller.banner.getBanners);
+
+  router.get(
+    '/api/OSSConfig/Banner',
+    controller.oss.getBannerUploadPathAndSignature
+  );
+  router.get(
+    '/api/OSSConfig/Apply',
+    controller.oss.getApplyUploadPathAndSignature
+  );
+  router.get(
+    '/api/OSSConfig/ScenicStatis',
+    controller.oss.getScenicStatisUploadPathAndSignature
+  );
 };
--- config/config.default.js
+++ config/config.default.js
@@ -20,6 +20,27 @@ module.exports = (appInfo) => {
     username: 'OAAdmin',
     password: 'w9E5F1Z9I6r1AHNA'
   };
+  // Aliyun OSS Config
+  config.aliyunOSS = {
+    accessKeyId: 'LTAI6eaMwrZ3cOof',
+    accessKeySecret: 'dKZQ0HoKvRDuCg3gZr0ryGhoYDCWSS',
+    bucket: 'test-dubhe-app',
+    endpoint: 'oss-cn-shanghai.aliyuncs.com',
+    policys: {
+      banner: {
+        expiration: 30,
+        dir: 'banner/'
+      },
+      apply: {
+        expiration: 30,
+        dir: 'apply/'
+      },
+      scenicStatis: {
+        expiration: 30,
+        dir: 'scenic-statis/'
+      }
+    }
+  };
 
   return config;
 };
