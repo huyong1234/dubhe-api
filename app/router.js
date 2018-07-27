af968b25666d1a5d9b540cb952ce8aad769f7e91
wenjiasen <wenjsen@gmail.com>
2018-07-27 18:03:53 +0800

    update

--- app/router.js
+++ app/router.js
@@ -6,21 +6,14 @@
 
 module.exports = (app) => {
   const { router, controller } = app;
-  router.resources('applyGroups', '/api/ApplyGroups', controller.applyGroup);
+
   router.get('/', controller.home.index);
   router.get('/api/banners', controller.banner.getBanners);
 
-  router.get(
-    '/api/OSSConfig/Banner',
-    controller.oss.getBannerUploadPathAndSignature
-  );
-  router.get(
-    '/api/OSSConfig/Apply',
-    controller.oss.getApplyUploadPathAndSignature
-  );
-  router.get(
-    '/api/OSSConfig/ScenicStatis',
-    controller.oss.getScenicStatisUploadPathAndSignature
-  );
+  router.resources('applyGroups', '/api/ApplyGroups', controller.applyGroup);
+
+  router.get('/api/OSSConfig/Banner', controller.oss.getBannerUploadPathAndSignature);
+  router.get('/api/OSSConfig/Apply', controller.oss.getApplyUploadPathAndSignature);
+  router.get('/api/OSSConfig/ScenicStatis', controller.oss.getScenicStatisUploadPathAndSignature);
   router.get('/api/ApplyGroups', controller.applyGroup.getApplyGroup);
 };
