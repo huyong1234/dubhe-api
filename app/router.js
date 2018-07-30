57507a30cb8dbad67a0a6893ca3698d6aa81bdc8
wenjiasen <wenjsen@gmail.com>
2018-07-30 11:42:40 +0800

    解决冲突

--- app/router.js
+++ app/router.js
@@ -10,25 +10,9 @@ module.exports = (app) => {
   router.get('/', controller.home.index);
   router.get('/api/banners', controller.banner.getBanners);
 
-<<<<<<< HEAD
-  router.get(
-    '/api/OSSConfig/Banner',
-    controller.oss.getBannerUploadPathAndSignature
-  );
-  router.get(
-    '/api/OSSConfig/Apply',
-    controller.oss.getApplyUploadPathAndSignature
-  );
-  router.get(
-    '/api/OSSConfig/ScenicStatis',
-    controller.oss.getScenicStatisUploadPathAndSignature
-  );
-  // router.get('/api/ApplyGroups', controller.applyGroup.getApplyGroup);
-=======
   router.resources('applyGroups', '/api/ApplyGroups', controller.applyGroup);
 
   router.get('/api/OSSConfig/Banner', controller.oss.getBannerUploadPathAndSignature);
   router.get('/api/OSSConfig/Apply', controller.oss.getApplyUploadPathAndSignature);
   router.get('/api/OSSConfig/ScenicStatis', controller.oss.getScenicStatisUploadPathAndSignature);
->>>>>>> 60e619e27d6c6374cf830586b32c40cf9ca2fb3b
 };
