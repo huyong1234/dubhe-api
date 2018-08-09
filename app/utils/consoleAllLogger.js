191e314e6f1706624132a2db9d893bb8f7fb733a
xiaoxin <1098015278@qq.com>
2018-08-09 15:12:43 +0800

    合并

--- app/utils/consoleAllLogger.js
+++ app/utils/consoleAllLogger.js
@@ -7,10 +7,7 @@ class ConsoleAllLogger extends Transport {
   // 定义 log 方法，在此方法中把日志上报给远端服务
   log(level, args) {
     let log;
-<<<<<<< HEAD
     console.debug('[ConsoleAllLogger] 进入docker日志管道');
-=======
->>>>>>> cf2f15eaa2cad7f1b3f6ee2858ce956bbe8e31f1
     if (args[0] instanceof Error) {
       const err = args[0];
       log = util.format('%s: %s\n%s\npid: %s\n', err.name, err.message, err.stack, process.pid);
