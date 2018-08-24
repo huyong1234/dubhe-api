472c5ab5287ed4290745445d402bd26a958d777b
xiaoxin <1098015278@qq.com>
2018-08-24 16:08:01 +0800

    错误状态吗大于等于500则不返回前端错误信息

--- config/config.default.js
+++ config/config.default.js
@@ -56,7 +56,10 @@ module.exports = (appInfo) => {
     all(err, ctx) {
       // 在此处定义针对所有响应类型的错误处理方法
       // 注意，定义了 config.all 之后，其他错误处理方法不会再生效
-      ctx.body = err.message;
+      // 错误状态吗大于等于500则不返回前端错误信息
+      if (this.status < 500) {
+        ctx.body = err.message;
+      }
     }
   };
 
