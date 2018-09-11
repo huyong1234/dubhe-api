69e55576672c8a949f096f4c6b6ce025e30da679
xiaoxin <1098015278@qq.com>
2018-09-11 15:11:41 +0800

    增加非空判断，完善逻辑

--- app/controller/department.js
+++ app/controller/department.js
@@ -12,7 +12,13 @@ class DepartmentController extends Controller {
     });
     // curl方法返回的是buffer对象，需要转换成json对象
     const info = JSON.parse(department.data);
-    this.ctx.body = info[0].deptList;
+    // 若info的长度等于0，则表示部门列表为空
+    if (info.length > 0) {
+      this.ctx.body = info[0].deptList;
+    } else {
+      // 如果该公司id下部门为空，则返回null
+      this.ctx.body = null;
+    }
   }
 }
 
